0e564bba3ec6393e6b5dd4c67b9187be31aac3fc
AndreyRalko <43684865+AndreyRalko@users.noreply.github.com>
2025-06-05 01:09:58 +0500

    2

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 // next.config.js
+
+const withNextIntl = require('next-intl/plugin')('./next-intl.config.ts');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-      serverActions: {}
-    }
-  };
-  
-  module.exports = nextConfig;
-  
+  experimental: {
+    serverActions: {}
+  }
+};
+
+module.exports = withNextIntl(nextConfig);
